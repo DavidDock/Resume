94f5a21c1f33342148ff83fad2bbf6ec73b37a41
DavidDock <>
2023-07-04 12:26:48 +0000

    add emailjs

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_z2yd1b7", "resume", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "enquiry_request": contactForm.enquirysummary.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;  // To block from loading a new page
+}
